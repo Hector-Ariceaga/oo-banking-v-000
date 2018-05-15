4977b0f3ad15e2e4e146bd5049ce4b556d7d5f05
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-15 17:39:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
       sender.balance -= self.amount
       receiver.balance += self.amount
       @status = 'complete'
+      binding.pry
     else 
        @status = 'rejected'
       return "Transaction rejection. Please check your acouunt balance."
